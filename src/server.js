31c64519e2e6cdd71e084af92e4fa06d50f580c1
Christopher Williams <cw@chriswillia.ms>
2019-08-21 17:21:33 -0500

    Implements knex for database connectivity in Express app.

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
+const knex = require('knex');
 const app = require('./app');
-const { PORT } = require('./config');
+const { PORT, DB_URL } = require('./config');
+
+const db = knex({
+    client: 'pg',
+    connection: DB_URL,
+});
+
+app.set('db', db);
 
 app.listen(PORT, () => {
     if (process.env.NODE_ENV === 'development') {
         console.log(`Server available at http://localhost:${PORT}`);
-    }
+    };
 });
